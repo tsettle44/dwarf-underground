b6d0fa4ecf6f379970b960be30e98f7c25e87870
Your Full Name <youremailaddress@example.com>
2017-05-22 14:04:03 -0400

    Added seperate Links Components

---
+++ src/Links/Link2/Link2.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Link2 extends Component {
+    render() {
+        return(
+            <div className="small-6 medium-3 columns other-article">
+                <a href="#">
+                    <img src="https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" alt="mountain" />
+                    <p>You won't believe what's under this mountain</p>
+                </a>  
+            </div>
+        )
+    }
+}
+
+export default Link2
--- src/Links/Links.js
+++ src/Links/Links.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import Link1 from './Link1/Link1.js'
+import Link2 from './Link2/Link2.js'
+import Link3 from './Link3/Link3.js'
+import Link4 from './Link4/Link4.js'
 import './Links.css'
 
 class Links extends Component {
@@ -6,30 +10,12 @@ class Links extends Component {
         return (
              <div className="small-12 columns other-articles">
                 <h2>From around the Realm</h2>
-                <div className="small-6 medium-3 columns other-article">
-                <a href="#">
-                    <img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc" />
-                    <p>Single Orcs in Indianapolis</p>
-                </a>  
-                </div>
-                <div className="small-6 medium-3 columns other-article">
-                <a href="#">
-                    <img src="https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" alt="mountain" />
-                    <p>You won't believe what's under this mountain</p>
-                </a>  
-                </div>
-                <div className="small-6 medium-3 columns other-article">
-                <a href="#">
-                    <img src="http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg" alt="gold" />
-                    <p>Mine 20% more gold with One Weird Trick</p>
-                </a> 
-                </div>
-                <div className="small-6 medium-3 columns other-article">
-                <a href="#">
-                    <img src="http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg" alt="hobbit" />
-                    <p>Surprise for Indiana Hobbits born before 1999</p>
-                </a> 
-                </div>
+
+                <Link1 />
+                <Link2 />
+                <Link3 />
+                <Link4 />
+                
           </div>            
         )
     }
