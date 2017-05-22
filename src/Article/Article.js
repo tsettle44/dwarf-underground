d57db500b316d2e8b1befccc64236c9043f339ff
Your Full Name <youremailaddress@example.com>
2017-05-22 14:04:21 -0400

    Added Article Button Components

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Buttons from './Buttons/Buttons'
 import './Article.css'
 
 class Header extends Component {
@@ -28,16 +29,8 @@ class Header extends Component {
 
                     <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
                     </section>
-                    <div className="article-links">
-                    <a className="article-link" href="#">
-                        <i className="fa fa-comments-o"></i>
-                        <span className="article-link-text">Comments</span>
-                    </a>
-                    <a className="article-link" href="#">
-                        <i className="fa fa-share"></i>
-                        <span className="article-link-text">Share Post</span>
-                    </a>
-            </div>
+                    
+                    <Buttons />
           </div>
         )
     }
