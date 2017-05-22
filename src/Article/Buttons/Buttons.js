911fa3f8b93b05f5b339d6330495cecf7cc898e8
Your Full Name <youremailaddress@example.com>
2017-05-22 14:52:36 -0400

    Add input when comment button clicked

--- src/Article/Buttons/Buttons.js
+++ src/Article/Buttons/Buttons.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react'
 import './Buttons.css'
 
 class Buttons extends Component {
+    constructor() {
+        super()
+        this.state = {
+            comments: [],
+        }
+    }
+    
+    CommentClick(ev) {
+        ev.preventDefault
+        const btn = ev.currentTarget
+        const articleTag = btn.parentElement
+        const input = articleTag.children[2]
+
+        input.setAttribute('type', 'text')
+
+        console.log(this.state.comments)
+    }
+
+    SubmitComment(ev) {
+        
+    }
+
     render() {
         return (
             <div className="article-links">
-                    <a className="article-link" href="#">
+                    <a className="article-link" href="#" onClick = {this.CommentClick.bind(this)}>
                         <i className="fa fa-comments-o"></i>
                         <span className="article-link-text">Comments</span>
                     </a>
@@ -13,6 +35,9 @@ class Buttons extends Component {
                         <i className="fa fa-share"></i>
                         <span className="article-link-text">Share Post</span>
                     </a>
+            <form onSubmit = {this.SubmitComment.bind(this)}>
+                <input className='input' type="hidden"></input>
+            </form>
             </div>
         )
     }
